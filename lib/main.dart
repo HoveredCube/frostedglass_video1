f5ff7da397a9aee6804bf526220a5f92bce51a49
Hovered Cube <68770458+HoveredCube@users.noreply.github.com>
2022-03-07 15:45:05 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,12 @@
 // ignore_for_file: prefer_typing_uninitialized_variables
+// script by Hovered Cube
+// This is the source code of HoveredCube video:
+// "How to create frostedGlass in flutter"
+// Tutorial Video link :
+// #
+// Follow @HoveredCube on instagram.
+
+
 import 'package:flutter/material.dart';
 import 'frostedglass.dart';
 
@@ -31,14 +39,22 @@ class MyHomePage extends StatelessWidget {
       body: Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
+            // Import image in pubspec.yaml first
             image: AssetImage('lib/images/background.jpg'),
+            // BoxFit.cover scales up the image until it completely covers the screen
             fit: BoxFit.cover,
           ),
         ),
+        // alignment is for centering the frostedglass, we don't use Center();
+        //  because if you want to change its position and remove the Center();
+        //  widget, everything will get messy the script crashes.
         alignment: Alignment.center,
         child: const FrostedGlassBox(
+          // theWidth is the width of the frostedglass
           theWidth: 200.0,
+          // theHeight is the height of the frostedglass
           theHeight: 200.0,
+          // theChild is the child of the frostedglass
           theChild: Text(
             'Hello world',
             style: TextStyle(color: Colors.white54, fontSize: 30.0),
@@ -48,3 +64,4 @@ class MyHomePage extends StatelessWidget {
     );
   }
 }
+
